ef0f0f848e5347aa792707acc2fd1cbea4f465a7
pozil <philippe.ozil@gmail.com>
2022-06-11 15:59:33 +0200

    Fixed async server config

--- src/server/server.js
+++ src/server/server.js
@@ -2,19 +2,25 @@ const configureServer = require('./index.js'),
     WebSocketService = require('./services/webSocketService.js'),
     LWR = require('lwr');
 
-// Configure server
-const lwrServer = LWR.createServer();
-const app = lwrServer.getInternalServer();
-const wss = new WebSocketService();
-configureServer(app, wss);
+async function start() {
+    // Configure server
+    const lwrServer = LWR.createServer();
+    const app = lwrServer.getInternalServer();
+    const wss = new WebSocketService();
+    await configureServer(app, wss);
 
-// HTTP and WebSocket Listen
-lwrServer
-    .listen(({ port, serverMode }) => {
-        console.log(`App listening on port ${port} in ${serverMode} mode\n`);
-        wss.connect(app);
-    })
-    .catch((err) => {
-        console.error(err);
-        process.exit(1);
-    });
+    // HTTP and WebSocket Listen
+    lwrServer
+        .listen(({ port, serverMode }) => {
+            console.log(
+                `App listening on port ${port} in ${serverMode} mode\n`
+            );
+            wss.connect(app);
+        })
+        .catch((err) => {
+            console.error(err);
+            process.exit(1);
+        });
+}
+
+start();
